7bdda8768c81fb1b850127b2eb1ab011216d60c3
KelvinTang <tangke3@oocl.com>
2025-09-04 19:22:04 +0800

    feat: added convertWordListToFrequencyMap

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -6,39 +6,20 @@ import java.util.stream.Collectors;
 public class WordFrequencyGame {
     public String getResult(String inputStr) {
 
-        List<String> words = List.of(inputStr.split("\\s+"));
+        List<String> wordList = List.of(inputStr.split("\\s+"));
 
-        if (words.size() == 1) {
-            return String.format("%s %d", words.get(0), 1);
+        if (wordList.size() == 1) {
+            return String.format("%s %d", wordList.get(0), 1);
         }
 
-        Map<String, Integer> frequencyMap = words.stream()
+        return "";
+    }
+
+    public Map<String, Integer> convertWordListToFrequencyMap(List<String> wordList) {
+        return wordList.stream()
                 .collect(Collectors.groupingBy(e -> e, Collectors.summingInt(e -> 1)));
-
-        Map<String, Integer> sortedMap = frequencyMap.entrySet()
-                .stream()
-                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-                .collect(Collectors.toMap(
-                        Map.Entry::getKey,
-                        Map.Entry::getValue,
-                        (e1, e2) -> e1, // Handle key collisions (not needed here)
-                        LinkedHashMap::new // Maintain insertion order
-                ));
-
-        return sortedMap.entrySet()
-                .stream()
-                .map(entry -> entry.getKey() + " " + entry.getValue())
-                .collect(Collectors.joining("\n"));
     }
 
-    public Map<Object, Long> convertWordsToMap(Map<Object, Long> map) {
-
-        map.entrySet().stream()
-                .sorted(Map.Entry.comparingByValue())
-                .forEach(System.out::println);
-
-        return map;
-    }
 
 
 }
