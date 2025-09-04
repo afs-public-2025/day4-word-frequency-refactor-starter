2c46abc71d0342905427b94c4e0b542cadfaa029
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-04 19:12:22 +0800

    feat:split inputStr into words

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,19 +1,15 @@
 import java.util.*;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr){
-
-        // apply stream api to count distinct word quantity
-        return Arrays.stream(inputStr.split("\\s+"))
-                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
-                .entrySet()
-                .stream()
-                .sorted((wordValueCountMap1,wordValueCountMap2)->wordValueCountMap2.getValue().intValue() - wordValueCountMap1.getValue().intValue())
-                .map(wordValueCountMap -> wordValueCountMap.getKey() + " " + wordValueCountMap.getValue())
-                .collect(Collectors.joining("\n"));
+    public String getResult(String inputStr) {
+        return null;
     }
 
+    private List<String> splitIntoWords(String inputStr) {
+        return Arrays.asList(inputStr.split("\\s+"));
+    }
 
 }
+
+
