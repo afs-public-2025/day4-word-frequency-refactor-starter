ae74fc192e8abe684c7ea740f114fdc47a1415fb
leo.meng <1336839379@qq.com>
2025-09-04 20:22:36 +0800

    fix: rename workFrequency class to WordFrequency

--- src/main/java/WordFrequency.java
+++ src/main/java/WordFrequency.java
@@ -1,12 +1,12 @@
-public class WorkFrequency {
-    private String word;
+public class WordFrequency {
+    private final String word;
     private int frequency;
 
-    public WorkFrequency(String word, int frequency){
+    public WordFrequency(String word, int frequency){
         this.word =word;
         this.frequency =frequency;
     }
-    
+
     public String getWord() {
         return this.word;
     }
