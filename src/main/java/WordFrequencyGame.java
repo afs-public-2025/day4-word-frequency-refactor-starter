f97296e3346155f57b7ce953000a8bab42176113
Lexi Zhong <zhongle5@oocl.com>
2025-09-04 19:17:58 +0800

    remove unused code

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,11 +1,10 @@
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.StringJoiner;
-import java.io.CharArrayWriter;
 
-import java.time.LocalDateTime;
+import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
     public String getResult(String inputStr){
@@ -16,61 +15,33 @@ public class WordFrequencyGame {
         } else {
 
             try {
-
-                //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
-
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map =getListMap(inputList);
-
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()){
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " +w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
+            return buildResult(covertToInput(countSameWords(inputStr.split("\\s+"))));
             } catch (Exception e) {
-
-
                 return "Calculate Error";
             }
         }
     }
 
-
-    private Map<String,List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input :  inputList){
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())){
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
-            }
-
-            else {
-                map.get(input.getValue()).add(input);
-            }
-        }
-
-
-        return map;
+    private Map<String,Long> countSameWords(String[] spitedWords) {
+        return Arrays.stream(spitedWords).collect(Collectors.groupingBy(s -> s, Collectors.counting()));
     }
 
+    private List<WordFrequency> covertToInput(Map<String,Long> map) {
+        List<WordFrequency> list = new ArrayList<>();
+        for (Map.Entry<String, Long> entry : map.entrySet()) {
+            WordFrequency wordFrequency = new WordFrequency(entry.getKey(), entry.getValue().intValue());
+            list.add(wordFrequency);
+        }
+        return list;
+    }
 
+    private String buildResult(List<WordFrequency> wordFrequencyList) {
+        wordFrequencyList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+        StringJoiner reu = new StringJoiner("\n");
+        for (WordFrequency wordFrequency : wordFrequencyList) {
+            String s = wordFrequency.getWord() + " " +wordFrequency.getWordCount();
+            reu.add(s);
+        }
+        return reu.toString();
+    }
 }
