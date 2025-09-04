7933e759bb7ebae05953c14f75cd95653a61d512
TAMNI <tamni@oocl.com>
2025-09-04 19:45:00 +0800

    style: remove unuse code and comment

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,38 +7,13 @@ import java.util.stream.Collectors;
 public class WordFrequencyGame {
     public String getResult(String inputStr){
 
-
-//        if (inputStr.split("\\s+").length==1) {
-//            return inputStr + " 1";
-//        } else {
-
             try {
 
-//                //split the input string with 1 to n pieces of spaces
-//                String[] words = inputStr.split("\\s+");
-////
-//
-//                List<WordFrequency> collect = Arrays.stream(words)
-//                        .map(word -> new WordFrequency(word, 1))
-//                        .collect(Collectors.toList());
-//
-//                Map<String, List<WordFrequency>> collect1 =
-//                        collect.stream().collect(Collectors.groupingBy(word -> word.getWord()));
-//
-//                //get the map for the next step of sizing the same word
-//                Map<String, List<WordFrequency>> map =getListMap(inputList);
-//
-//                List<WordFrequency> list = new ArrayList<>();
-//                for (Map.Entry<String, List<WordFrequency>> entry : map.entrySet()){
-//                    WordFrequency input = new WordFrequency(entry.getKey(), entry.getValue().size());
-//                    list.add(input);
-//                }
-//                inputList = list;
 
                 Map<String, List<WordFrequency>> frequencyMap = countWordFrequency(inputStr);
-                List<WordFrequency> inputList = calculatedWordFrequency(frequencyMap);
+                List<WordFrequency> frequencyList = calculatedWordFrequency(frequencyMap);
 
-                return sortedOutput(inputList);
+                return sortedOutput(frequencyList);
             } catch (Exception e) {
 
 
@@ -68,33 +43,14 @@ public class WordFrequencyGame {
                 .collect(Collectors.toList());
     }
 
-    private String sortedOutput (List<WordFrequency> inputList){
+    private String sortedOutput (List<WordFrequency> frequencyList){
 
-        return inputList.stream()
+        return frequencyList.stream()
                 .sorted((w1, w2) -> Integer.compare(w2.getWordCount(), w1.getWordCount()))
                 .map(w -> w.getWord() + " " + w.getWordCount())
                 .collect(Collectors.joining("\n"));
     }
 
-
-//    private Map<String,List<WordFrequency>> getListMap(List<WordFrequency> frequencyList) {
-//        Map<String, List<WordFrequency>> map = new HashMap<>();
-//        for (WordFrequency input :  frequencyList){
-////       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-//            if (!map.containsKey(input.getWord())){
-//                ArrayList arr = new ArrayList<>();
-//                arr.add(input);
-//                map.put(input.getWord(), arr);
-//            }
-//
-//            else {
-//                map.get(input.getWord()).add(input);
-//            }
-//        }
-//
-//
-//        return map;
-//    }
 
 
 }
