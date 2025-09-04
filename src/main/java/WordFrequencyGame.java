1683673e21b7b0f0d66d181ded83df1693ef454a
Andy Hui <chhuiwork@gmail.com>
2025-09-04 18:26:38 +0800

    refactor: Refactor main function to use frequncy map for result output

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,29 +5,32 @@ import java.util.stream.Collectors;
 public class WordFrequencyGame {
     public String getResult(String inputStr){
         try {
-            List<String> extractedWords = Arrays.stream(inputStr.split("\\s+")).toList();
+          List<String> extractedWords = Arrays.stream(inputStr.split("\\s+")).toList();
 
-            List<Input> inputList = new ArrayList<>();
-            for (String s : extractedWords) {
-                Input input = new Input(s, 1);
-                inputList.add(input);
-            }
-
-            //get the map for the next step of sizing the same word
-            Map<String, List<Input>> map = getListMap(inputList);
-
-            List<Input> list = new ArrayList<>();
-            for (Map.Entry<String, List<Input>> entry : map.entrySet()){
-                Input input = new Input(entry.getKey(), entry.getValue().size());
-                list.add(input);
-            }
-            inputList = list;
-
-            inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+          Map<String, Long> frequencyMap = buildWordFrequencyMap(extractedWords);
+          frequencyMap = sortWordFrequencyMap(frequencyMap);
+//
+//            List<Input> inputList = new ArrayList<>();
+//            for (String s : extractedWords) {
+//                Input input = new Input(s, 1);
+//                inputList.add(input);
+//            }
+//
+//            //get the map for the next step of sizing the same word
+//            Map<String, List<Input>> map = getListMap(inputList);
+//
+//            List<Input> list = new ArrayList<>();
+//            for (Map.Entry<String, List<Input>> entry : map.entrySet()){
+//                Input input = new Input(entry.getKey(), entry.getValue().size());
+//                list.add(input);
+//            }
+//            inputList = list;
+//
+//            inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
             StringJoiner joiner = new StringJoiner("\n");
-            for (Input w : inputList) {
-                String s = w.getValue() + " " +w.getWordCount();
+            for (Map.Entry<String, Long> wordToFrequency: frequencyMap.entrySet()) {
+                String s = wordToFrequency.getKey() + " " + wordToFrequency.getValue();
                 joiner.add(s);
             }
             return joiner.toString();
