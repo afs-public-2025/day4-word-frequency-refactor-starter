9bd3e1fc298ec285c53e6335be65b43351b6f6cd
zhuhao <zhuhao4396@foxmail.com>
2025-09-04 20:30:57 +0800

    删除if else，对一些奇怪的变量名重命名

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,33 +1,23 @@
 import java.util.*;
 
 public class WordFrequencyGame {
-    public String getResult(String inputStr){
-
-
-        if (inputStr.split("\\s+").length==1) {
-            return inputStr + " 1";
-        } else {
-
-            try {
-                String[] splitArray = inputStr.split("\\s+");
-                Map<String, Integer> countMap = new HashMap<>();
-                for (String s : splitArray) {
-                    countMap.merge(s, 1, Integer::sum);
-                }
-                List<Map.Entry<String, Integer>> entries = new ArrayList<>(countMap.entrySet());
-                entries.sort((e1, e2) -> e2.getValue() - e1.getValue());
-
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Map.Entry<String, Integer> map : entries) {
-                    String s = map.getKey() + " " + map.getValue();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
+    public String getResult(String inputString) {
+        try {
+            Map<String, Integer> countMap = new HashMap<>();
+            for (String s : inputString.split("\\s+")) {
+                countMap.merge(s, 1, Integer::sum);
             }
+            List<Map.Entry<String, Integer>> entries = new ArrayList<>(countMap.entrySet());
+            entries.sort((e1, e2) -> e2.getValue() - e1.getValue());
+
+            StringJoiner result = new StringJoiner("\n");
+            for (Map.Entry<String, Integer> map : entries) {
+                String s = map.getKey() + " " + map.getValue();
+                result.add(s);
+            }
+            return result.toString();
+        } catch (Exception e) {
+            return "Calculate Error";
         }
     }
-
-
 }
