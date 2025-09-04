8d4ebe78f5792110abab7c81957e06868969981f
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-04 19:12:51 +0800

    feat:calculate word frequencies

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -10,6 +10,20 @@ public class WordFrequencyGame {
         return Arrays.asList(inputStr.split("\\s+"));
     }
 
+    private List<Word> calculateWordFrequencies(List<String> words) {
+        Map<String, Integer> wordCountMap = new HashMap<>();
+
+        for (String word : words) {
+            wordCountMap.put(word, wordCountMap.getOrDefault(word, 0) + 1);
+        }
+
+        List<Word> wordFrequencies = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordCountMap.entrySet()) {
+            wordFrequencies.add(new Word(entry.getKey(), entry.getValue()));
+        }
+
+        return wordFrequencies;
+    }
 }
 
 
