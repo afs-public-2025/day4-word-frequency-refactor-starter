52d5cdc2ce675e6fd2b90acd08dc02b597eadda8
Jonny Zhou <jonny.zhou@oocl.com>
2025-09-05 09:29:56 +0800

    fix:fig logic bug and make it more beautiful

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -7,6 +7,7 @@ public class WordFrequencyGame {
 
         // apply stream api to count distinct word quantity
         return Arrays.stream(inputStr.split("\\s+"))
+                .map(String::toLowerCase)
                 .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
                 .entrySet()
                 .stream()
